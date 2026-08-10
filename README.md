772e18ad5bda947ebd3cf100604f11aa46498faa
Hyunsik (Harry) Yoon <yhski11@gmail.com>
2026-08-10 12:25:41 +0900

    Load predictor data and construct initial variables

---
+++ README.md
@@ -0,0 +1,13 @@
+# Campbell and Thompson (2008) Replication
+
+Replication of:
+
+Campbell, J. Y., & Thompson, S. B. (2008).
+"Predicting Excess Stock Returns Out of Sample:
+Can Anything Beat the Historical Average?"
+Review of Financial Studies, 21(4), 1509–1531.
+
+## Objective
+
+Replicate the main out-of-sample stock return forecasting
+results of Campbell and Thompson (2008) using Python.
---
+++ src/01_load_and_prepare_data.py
@@ -0,0 +1,36 @@
+#Import the pandas library so we can work with spreadsheet style data
+import pandas as pd
+
+# Store the location of our raw Excel dataset
+file_path = "../data/raw/PredictorData.xls"
+
+# Read the "Monthly" sheet from Excel workbook into a Pandas DataFrame
+df = pd.read_excel(file_path, sheet_name = "Monthly")
+
+# Print the first five rows so we can check the data loaded correctly
+print(df.head())
+
+# -------------------------------
+# Construct replication variables
+# -------------------------------
+
+# Construct the dividend-price ratio
+df["dp"] = df["D12"] / df["Index"]
+
+# Construct the earnings-price ratio.
+df["ep"] = df["E12"] / df["Index"]
+
+# Construct the term spread.
+df["term_spread"] = df["lty"] - df["tbl"]
+
+# Construct the realized excess stock return.
+df["excess_return"] = df["CRSP_SPvw"] - df["Rfree"]
+
+# Print the first five rows of the constructed variables to check they were created correctly.
+print(df[["dp", "ep", "term_spread", "excess_return"]].head())
+
+# Select observations from January 1927 and onward. Think df[ CONDITION ] = "Give me the rows of df that satisfy CONDITION."
+df_1927 = df[df["yyyymm"] >= 192701]
+
+# Print the first five observations from 1927 onward to check the constructed variables.
+print(df_1927[["yyyymm", "dp", "ep", "term_spread", "excess_return"]].head())
